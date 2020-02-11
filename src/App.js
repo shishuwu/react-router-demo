a8b470f6a360b726422f563ffed6b29ab9464c35
shishuwu <shuwu.shi@gmail.com>
2020-02-11 17:38:08 +0800

    nested-routing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useRouteMatch,
+  useParams
 } from 'react-router-dom';
 
 function App() {
@@ -20,7 +22,7 @@ function App() {
               <Link to="/about">About</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/topics">Topics</Link>
             </li>
           </ul>
         </nav>
@@ -32,8 +34,8 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/topics">
+            <Topics />
           </Route>
           <Route path="/">
             <Home />
@@ -51,8 +53,42 @@ function About() {
   return <h2>About</h2>;
 }
 
-function Users(){
-  return <h2>Users</h2>
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>)
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>
 }
 
 export default App;
